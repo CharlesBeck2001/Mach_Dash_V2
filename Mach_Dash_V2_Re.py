15b6b4923b74028539f7a0f3f85c72ff266c204d
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2025-01-11 16:52:46 -0500

    Update Mach_Dash_V2_Re.py

--- Mach_Dash_V2_Re.py
+++ Mach_Dash_V2_Re.py
@@ -13,6 +13,8 @@ import plotly.express as px
 import altair as alt
 import plotly.graph_objects as go
 
+supabase_url = "https://fzkeftdzgseugijplhsh.supabase.co"
+supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6a2VmdGR6Z3NldWdpanBsaHNoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMjcxMzk3NCwiZXhwIjoyMDQ4Mjg5OTc0fQ.Og46ddAeoybqUavWBAUbUoj8HJiZrfAQZi-6gRP46i4"
 
 def execute_sql(query):
     headers = {
