a3bd9eb926ebb2bf702dfa772383ff4cbad94932
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2025-01-15 15:14:13 -0500

    Update Mach_Dash_V2_Re.py

--- Mach_Dash_V2_Re.py
+++ Mach_Dash_V2_Re.py
@@ -1505,11 +1505,11 @@ else:
 
 
 # Initialize session state if not already present
-if 'df_trade_address' not in st.session_state:
-    st.session_state.df_trade_address = df_trade_address
-    st.session_state.df_volume_address = df_volume_address
-    st.session_state.page_trade = 0
-    st.session_state.page_volume = 0
+#if 'df_trade_address' not in st.session_state:
+    #st.session_state.df_trade_address = df_trade_address
+    #st.session_state.df_volume_address = df_volume_address
+    #st.session_state.page_trade = 0
+    #st.session_state.page_volume = 0
 
 # Pagination function
 @st.cache_data
