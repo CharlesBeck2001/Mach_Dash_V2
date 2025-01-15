32085cc7a5c10f30371dac67c90a2183a6f00ae2
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2025-01-15 15:16:06 -0500

    Update Mach_Dash_V2_Re.py

--- Mach_Dash_V2_Re.py
+++ Mach_Dash_V2_Re.py
@@ -2182,13 +2182,13 @@ st.subheader("User Portion by Volume")
 st.plotly_chart(fig, use_container_width=True)
 
 # Add percentage columns to session state dataframes
-df_trade_address['percentage_of_total_trades'] = (
-    df_trade_address['trade_count'] / (2*total_trades) * 100
-).round(2)
+#df_trade_address['percentage_of_total_trades'] = (
+#    df_trade_address['trade_count'] / (2*total_trades) * 100
+#).round(2)
 
-df_volume_address['percentage_of_total_volume'] = (
-    df_volume_address['total_user_volume'] / (2*total_volume) * 100
-).round(2)
+#df_volume_address['percentage_of_total_volume'] = (
+#    df_volume_address['total_user_volume'] / (2*total_volume) * 100
+#).round(2)
 
 
 trade_add_query = f"""
