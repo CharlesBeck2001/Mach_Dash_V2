d4fa20294cab2983670650cf64c6da1a7e448ef2
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2025-01-16 11:31:01 -0500

    Update Mach_Dash_V2_Re.py

--- Mach_Dash_V2_Re.py
+++ Mach_Dash_V2_Re.py
@@ -1437,8 +1437,8 @@ selected_assets = st.multiselect("Select Assets", asset_list, default=asset_list
 all_assets_data = pd.DataFrame()
 
 
-st.write(pd.to_datetime(start_date_2))
-st.write(pd.to_datetime(st.session_state["preloaded_2"]['Total Daily Value']['day']))
+#st.write(pd.to_datetime(start_date_2))
+#st.write(pd.to_datetime(st.session_state["preloaded_2"]['Total Daily Value']['day']))
 
 col1, col2 = st.columns(2)
 with col2:
