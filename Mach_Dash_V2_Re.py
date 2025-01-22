72c43e420b94a923c63dd5724075aad19a28c726
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2025-01-21 22:07:34 -0500

    Update Mach_Dash_V2_Re.py

--- Mach_Dash_V2_Re.py
+++ Mach_Dash_V2_Re.py
@@ -2138,6 +2138,7 @@ with col1:
     # Pivot the data to have separate columns for each asset
     pivot_data = all_assets_data_hour.pivot(index='date', columns='asset', values='total_hourly_volume')
     pivot_data = pivot_data.fillna(0)
+    pivot_data = pivot_data.reset_index()
     st.write(pivot_data)
     # Melt the data back into long format for Plotly
     melted_data = pivot_data.melt(id_vars=['date'], var_name='asset', value_name='total_hourly_volume')
@@ -2189,11 +2190,11 @@ with col2:
     # Pivot the data to have separate columns for each asset
     pivot_data = all_assets_data_day.pivot(index='day', columns='asset', values='total_daily_volume')
     pivot_data = pivot_data.fillna(0)
-    
+    pivot_data = pivot_data.reset_index()
     # Reset index to make it Plotly-compatible
 
     # Melt the data back into long format for Plotly
-    melted_data = pivot_data.reset_index().melt(id_vars='date', var_name='Asset', value_name='Total Hourly Volume')
+    melted_data = pivot_data.melt(id_vars='date', var_name='asset', value_name='Total Daily Volume')
 
     # Create an interactive bar chart with Plotly
     fig = px.bar(
