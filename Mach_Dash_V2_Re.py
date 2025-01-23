9f0c56123799a84accb9804d36ebe2a45950962c
CharlesBeck2001 <58281703+CharlesBeck2001@users.noreply.github.com>
2025-01-23 14:13:39 -0500

    Update Mach_Dash_V2_Re.py

--- Mach_Dash_V2_Re.py
+++ Mach_Dash_V2_Re.py
@@ -1106,7 +1106,6 @@ if "preloaded" not in st.session_state:
         date = today - timedelta(days=i)
         date = date.strftime('%Y-%m-%dT%H:%M:%S')
 
-        
         data = stats_box_maker(date)
         preloaded[i] = data
     
@@ -1130,6 +1129,7 @@ if time_ranges[selected_range] is not None:
     load_metrics(st.session_state["preloaded"][time_ranges[selected_range]])
     date = today - timedelta(days=time_ranges[selected_range])
     date = date.strftime('%Y-%m-%dT%H:%M:%S')
+    st.write(date)
     
 else:
     load_metrics(st.session_state["preloaded"][0])
